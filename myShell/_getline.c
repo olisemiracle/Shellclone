98ae16f7943dc4622c52a213b2bd18151e0b263d
olisemiracle <olisemiracle904@gmail.com>
2023-02-13 19:13:40 +0100

    getline

--- myShell/_getline.c
+++ myShell/_getline.c
@@ -1,14 +1,14 @@
 #include "main.h"
 size_t _getline(char **buffer, size_t *n)
 {
-	char line[1024];
+	char line[256];
 	ssize_t rbytes;
 	size_t t = 0, t2 = 0, t3 = 0;
 
 	if (buffer == NULL || n == NULL)
 		return (-1);
 
-	while (t2 == 0 && (rbytes = read(STDIN_FILENO, line, 1024 -1)))
+	while (t2 == 0 && (rbytes = read(STDIN_FILENO, line, 256 - 1)))
 	{
 		if (rbytes == -1)
 			return(-1);
@@ -18,17 +18,19 @@ size_t _getline(char **buffer, size_t *n)
 		{
 			if (line[t3] == '\n')
 				t2 = 1;
-			t3++;
+			break;
 		}
 		if (t != 0)
 		{
-			*n = *n + rbytes;
+			*n = *n + rbytes + 1;
 			*buffer = realloc(*buffer, *n);
 			if (*buffer == NULL)
 			{
+				free(*buffer);
 				return (-1);
 			}
 			*buffer= strncat(*buffer, line, rbytes);
+			*buffer[*n] = '\0';
 			if (*buffer == NULL)
 				return(-1);
 		}
@@ -41,6 +43,7 @@ size_t _getline(char **buffer, size_t *n)
 			*buffer = strncpy(*buffer, line, rbytes);
 			if (*buffer == NULL)
 				return (-1);
+			*buffer[rbytes] = '\0';
 			*n = rbytes;
 			t = 1;
 		}
